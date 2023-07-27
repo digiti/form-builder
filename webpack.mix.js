772634621f95ca1e83004073cf9c9b698703d859
Jasper Van de Vloet <>
2023-07-27 15:51:26 +0200

    webpack setup + bootstrap + livewire 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,37 @@
 const mix  = require('laravel-mix');
+require('laravel-mix-purgecss');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
-mix.browserSync('conversion-tool.test');
+ /**
+  * Javascript
+  */
+mix.js('resources/js/site.js', 'public/js');
+
+/**
+ * Stylesheets
+ */
+mix.sass('resources/scss/site.scss', 'public/css')
+   .purgeCss({
+        enabled: false
+    });
+mix.options({
+  processCssUrls: false
+});
+
+/**
+ * Browser sync
+ */
+ mix.browserSync('conversion-tool.test');
+
+if (mix.inProduction()) {
+   mix.version();
+}
