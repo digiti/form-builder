d3c57f2b2ee1107dd639cda5be57f30ce354103b
Jasper Van de Vloet <>
2023-08-30 15:31:03 +0200

    Content items direct in step

--- resources/views/livewire/framework/layout/step.blade.php
+++ resources/views/livewire/framework/layout/step.blade.php
@@ -24,6 +24,16 @@
     @foreach ($object->getSchema() as $object)
         @if ($this->isRow($object))
             <x-content.row :$object />
+        @elseif ($this->isImage($object))
+            <x-content.image :$object />
+        @elseif ($this->isParagraph($object))
+            <x-content.paragraph :$object />
+        @elseif ($this->isHeading($object))
+            <x-content.heading :$object />
+        @elseif ($this->isHtml($object))
+            <x-content.html :$object />
+        @elseif ($this->isAnchor($object))
+            <x-content.anchor :$object />
         @else
             <livewire:is :component="$object->getView()" ::key="md5($loop - > index)" :$object :$result />
         @endif
